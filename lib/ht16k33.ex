cfeaac4cd01721701366d753d30e9d3c32f7463b
Marcel Kessler <marcel.kessler@posteo.de>
2018-02-11 20:08:40 +0100

    Elixir boilerplate

---
+++ lib/ht16k33.ex
@@ -0,0 +1,18 @@
+defmodule Ht16k33 do
+  @moduledoc """
+  Documentation for Ht16k33.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ht16k33.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ht16k33_test.exs
@@ -0,0 +1,8 @@
+defmodule Ht16k33Test do
+  use ExUnit.Case
+  doctest Ht16k33
+
+  test "greets the world" do
+    assert Ht16k33.hello() == :world
+  end
+end
